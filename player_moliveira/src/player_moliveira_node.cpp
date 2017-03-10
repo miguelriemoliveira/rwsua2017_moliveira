c57eab50b31c133042a0deda7e9dbf3afe59964e
Miguel Armando Riem de Oliveira <m.riem.oliveira@gmail.com>
2017-03-10 12:17:26 +0000

    Close #1. Created a lib and msg packages

--- player_moliveira/src/player_moliveira_node.cpp
+++ player_moliveira/src/player_moliveira_node.cpp
@@ -1,12 +1,14 @@
+//ATD includes
 #include <iostream>
 #include <vector>
 
-#include <rwsua2017_libs/player.h>
+//ROS INCLUDES
+#include <ros/ros.h>
 
-//test include rws message
+//Local includes
+#include <rwsua2017_libs/player.h>
 #include <rwsua2017_msgs/MakeAPlay.h>
 
-//indent your code!
 
 using namespace std;
 
@@ -16,21 +18,37 @@ namespace rwsua2017
   class MyPlayer: public Player
   {
     public:
+      ros::NodeHandle n;
+      int a;
+      ros::Subscriber sub;
 
     MyPlayer(string argin_name, string argin_team_name): Player(argin_name, argin_team_name)
     {
+      sub = n.subscribe("/make_a_play", 1000, &MyPlayer::makeAPlayCallback, this);
+
+      a = 7;
+      int b = 5;
+
       cout << "Initialized MyPlayer" << endl;
     };
-  
-    vector<string> teammates;
-  
+
+      void makeAPlayCallback(const rwsua2017_msgs::MakeAPlay::ConstPtr& msg)
+      {
+        cout << "received a make a play msg with max_displacement = " << msg->max_displacement << endl;
+      }
+
+      vector<string> teammates;
+
   };
 }
 
 
-int main()
+int main(int argc, char **argv)
 {
   cout << "Hello world" << endl;           
+
+  ros::init(argc, argv, "player_moliveira");
+
 
   rwsua2017::MyPlayer myplayer("player_moliveira", "green");
 
@@ -39,7 +57,7 @@ int main()
 
   myplayer.teammates.push_back("fsilva");
   myplayer.teammates.push_back("vsilva");
-    
+
   //ciclo for tipico em c
   //int i;
   //for (i=0; i < 5; i = i + 1)
@@ -50,6 +68,7 @@ int main()
     cout << myplayer.teammates[i] << endl; 
   }
 
+  ros::spin();
 
   return 1;                                                      
 }
