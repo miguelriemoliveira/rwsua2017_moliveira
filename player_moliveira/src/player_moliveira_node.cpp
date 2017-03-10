43ff38cc04f20d5979b93b6fc004b917e46fda0f
Miguel Armando Riem de Oliveira <m.riem.oliveira@gmail.com>
2017-03-10 16:01:55 +0000

    visualization and tfs

--- player_moliveira/src/player_moliveira_node.cpp
+++ player_moliveira/src/player_moliveira_node.cpp
@@ -4,6 +4,8 @@
 
 //ROS INCLUDES
 #include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
 
 //Local includes
 #include <rwsua2017_libs/player.h>
@@ -12,6 +14,8 @@
 
 using namespace std;
 using namespace boost;
+using namespace tf;
+using namespace ros;
 
 namespace rwsua2017
 {
@@ -21,16 +25,20 @@ namespace rwsua2017
     public:
 
       //PROPPERTIES
-      ros::Subscriber sub;
+      Subscriber sub;
+      TransformBroadcaster br;
+      Transform t1;
 
       MyPlayer(string argin_name, string argin_team_name): Player(argin_name, argin_team_name)
     {
-
-
-
       //Subscribe tyo the make_a_play_message
       sub = n.subscribe("/make_a_play", 1000, &MyPlayer::makeAPlayCallback, this);
 
+        t1.setOrigin( tf::Vector3(1, 1, 0.0) );
+        Quaternion q;
+        q.setRPY(0, 0, 0);
+        t1.setRotation(q);
+        br.sendTransform(tf::StampedTransform(t1, ros::Time::now(), "map", name));
 
       cout << "Initialized MyPlayer" << endl;
     };
@@ -39,6 +47,23 @@ namespace rwsua2017
       void makeAPlayCallback(const rwsua2017_msgs::MakeAPlay::ConstPtr& msg)
       {
         cout << "received a make a play msg with max_displacement = " << msg->max_displacement << endl;
+
+
+        //Definicao dos angulos de rotação e valores de translação 
+        //DEVERIA SER CALCULADO PELA AI DO SISTEMA
+        float turn_angle = M_PI/10;
+        float displacement = 0.5;
+
+        //Compute the new reference frame
+        tf::Transform t_mov;
+        Quaternion q;
+        q.setRPY(0, 0, turn_angle);
+        t_mov.setRotation(q);
+        t_mov.setOrigin( Vector3(displacement , 0.0, 0.0) );
+
+        tf::Transform t = t1  * t_mov;
+        //Send the new transform to ROS
+        br.sendTransform(StampedTransform(t, ros::Time::now(), "/map", name));
       }
 
       vector<string> teammates;
