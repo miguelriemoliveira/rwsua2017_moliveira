ed33c7b0f842051969e1993c05c91ae632459b5f
Miguel Armando Riem de Oliveira <m.riem.oliveira@gmail.com>
2017-03-10 14:43:13 +0000

    new version of library

--- rwsua2017_libs/include/rwsua2017_libs/player.h
+++ rwsua2017_libs/include/rwsua2017_libs/player.h
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <vector>
 
+//ROS INCLUDES
+#include <ros/ros.h>
 
 using namespace std;
+using namespace boost;
 
 namespace rwsua2017
 {
@@ -10,14 +13,98 @@ namespace rwsua2017
   class Player
   {
     public:
+      ros::NodeHandle n;
+      shared_ptr< vector<string> > red_team;
+      shared_ptr< vector<string> > green_team;
+      shared_ptr< vector<string> > blue_team;
+      shared_ptr< vector<string> > my_team;
+      shared_ptr< vector<string> > preys_team;
+      shared_ptr< vector<string> > hunters_team;
 
       Player(string argin_name, string argin_team_name = "blue")
       {
         cout << "player name " << argin_name << endl;   
         this->name = argin_name;
-        set_team_name(argin_team_name);
+
+        //Create the three teams
+        red_team = (shared_ptr<vector <string> >) new vector<string>();
+        green_team = (shared_ptr<vector <string> >) new vector<string>();
+        blue_team = (shared_ptr<vector <string> >) new vector<string>();
+
+        //ler parametrso com lista de jogadores por equipa
+        n.getParam("red", *red_team);
+        n.getParam("green", *green_team);
+        n.getParam("blue", *blue_team);
+
+
+        printInfo();
+
+        //set_team_name(argin_team_name);
 
       }
+
+      void findMyTeam(void)
+      {
+        for (size_t i = 0; i < red_team->size(); ++i)
+        {
+          if (red_team->at(i) == name)
+          {
+            my_team = red_team;
+            preys_team = green_team;
+            hunters_team = blue_team;
+            cout << "I am from team red" << endl;
+          }
+        }
+
+        for (size_t i = 0; i < green_team->size(); ++i)
+        {
+          if (green_team->at(i) == name)
+          {
+            my_team = green_team;
+            preys_team = blue_team;
+            hunters_team = red_team;
+            cout << "I am from team green" << endl;
+          }
+        }
+
+        for (size_t i = 0; i < blue_team->size(); ++i)
+        {
+          if (blue_team->at(i) == name)
+          {
+            my_team = blue_team;
+            preys_team = red_team;
+            hunters_team = green_team;
+            cout << "I am from team blue" << endl;
+          }
+        }
+
+
+      }
+
+
+
+      void printInfo(void)
+      {
+        cout << "red team:" << endl; 
+        for (size_t i = 0; i < red_team->size(); ++i)
+        {
+          cout << red_team->at(i) << endl; 
+        }
+
+        cout << "green team:" << endl; 
+        for (size_t i = 0; i < green_team->size(); ++i)
+        {
+          cout << green_team->at(i) << endl; 
+        }
+
+        cout << "blue team:" << endl; 
+        for (size_t i = 0; i < blue_team->size(); ++i)
+        {
+          cout << blue_team->at(i) << endl; 
+        }
+      }
+
+
 
       string name;
 
@@ -32,8 +119,8 @@ namespace rwsua2017
       void set_team_name(void) {set_team_name("red");}
       string get_team_name(void){return this->team_name;};
 
-      private:
-        string team_name;
+    private:
+      string team_name;
 
   };
 
